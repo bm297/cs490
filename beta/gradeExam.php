45edc26537b58436d482ff02371573114999ad8e
ab674 <ab674@njit.edu>
2017-02-20 18:00:04 -0500

    Add files via upload

---
+++ beta/gradeExam.php
@@ -0,0 +1,160 @@
+<?php
+
+$testcategory = "recursion";
+$testname = "factorial";
+$testanswer = "def factorial(number):
+  if number < 2:
+    return 1
+  return number * factorial(number - 1)";
+$testtestcase = 5;
+$testtestcaseresult = 120;
+
+$capture = array(
+  "category" => $testcategory,
+  "name" => $testname,
+  "answer" => $testanswer,
+  "testcase" => $testtestcase,
+  "testcaseresult" => $testtestcaseresult
+);
+
+// $capture = json_decode(file_get_contents("php://input"), true);
+$difficulty = $capture["difficulty"];
+$category = $capture["category"];
+$name = $capture["name"];
+$answer = $capture["answer"];
+$testcase = $capture["testcase"];
+$testcaseresult = $capture["testcaseresult"];
+$grade = 0;
+$feedback = "";
+$file = "temporary.py";
+file_put_contents($file, $answer, FILE_APPEND);
+
+$namecheck = strpos($answer, $name);
+if ($namecheck === false) {
+  $grade += 0;
+  $feedback .= "Incorrect name (+0)\n";
+}
+else {
+  $grade += 1;
+  $feedback .= "Correct name (+1)\n";
+}
+
+if ($category == "recursion") {
+  $identifiercount = substr_count($answer, $name);
+  if ($identifiercount > 1) {
+    $grade += 1;
+    $feedback .= "Recursive (+1)\n";
+  }
+  else {
+    $grade += 0;
+    $feedback .= "Not recursive (+0)\n";
+  }
+}
+else if ($category == "forloop") {
+  $identifiercount = substr_count($answer, "for");
+  if ($identifiercount > 0) {
+    $grade += 1;
+    $feedback .= "For loop (+1)\n";
+  }
+  else {
+    $grade += 0;
+    $feedback .= "No for loop (+0)\n";
+  }
+}
+else if ($category == "whileloop") {
+  $identifiercount = substr_count($answer, "while");
+  if ($identifiercount > 0) {
+    $grade += 1;
+    $feedback .= "While loop (+1)\n";
+  }
+  else {
+    $grade += 0;
+    $feedback .= "No while loop (+0)\n";
+  }
+}
+else if ($category == "conditional") {
+  $identifiercount = substr_count($answer, "if");
+  if ($identifiercount > 0) {
+    $grade += 1;
+    $feedback .= "Conditional (+1)\n";
+  }
+  else {
+    $grade += 0;
+    $feedback .= "No Conditional (+0)\n";
+  }
+}
+else if ($category == "dictionary") {
+  $identifiercount = substr_count($answer, "{");
+  if ($identifiercount > 0) {
+    $grade += 1;
+    $feedback .= "Dictionary (+1)\n";
+  }
+  else {
+    $grade += 0;
+    $feedback .= "No dictionary (+0)\n";
+  }
+}
+else if ($category == "indexing") {
+  $identifiercount = substr_count($answer, "[");
+  if ($identifiercount > 0) {
+    $grade += 1;
+    $feedback .= "Indexing used (+1)\n";
+  }
+  else {
+    $grade += 0;
+    $feedback .= "No indexing used (+0)\n";
+  }
+}
+else {
+  $grade += 0;
+}
+
+$returncheck = strpos($answer, "return");
+if ($returncheck === false) {
+  $grade += 0;
+  $feedback .= "No result returned (+0)\n";
+}
+else {
+  $grade += 1;
+  $feedback .= "Result returned (+1)\n";
+}
+
+file_put_contents($file, "\n\n", FILE_APPEND);
+file_put_contents($file, "print($name($testcase))", FILE_APPEND);
+
+$output = shell_exec("python temporary.py");
+if ($output == $testcaseresult) {
+  $grade += 2;
+  $feedback .= "Code compiles (+1)\n";
+  $feedback .= "Correct result (+1)";
+}
+else if ($output > 0) {
+  $grade += 1;
+  $feedback .= "Code compiles (+1)\n";
+  $feedback .= "Incorrect result (+0)";
+}
+else {
+  $grade -= 0;
+  $feedback .= "Code does not compiles (-1)";
+}
+
+$capture["grade"] = $grade;
+$capture["feedback"] = $feedback;
+// $capture = json_encode($capture);
+
+echo $capture["feedback"];
+echo "\n";
+echo $capture["grade"];
+echo "\n";
+
+// file_put_contents($file, "");
+
+// $connection = curl_init();
+// curl_setopt($connection, CURLOPT_URL, "https://web.njit.edu/~bm297/cs490/gradedQuestion.php");
+// curl_setopt($connection, CURLOPT_POST, 1);
+// curl_setopt($connection, CURLOPT_POSTFIELDS, $capture);
+// curl_setopt($connection, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+// $designation = curl_exec($connection);
+// curl_close($connection);
+
+?>
